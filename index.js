78c22b8e69ffa6040944fef7abdebd877a303b4a
Noor Sulaiman <noor.s.suliman@gmail.com>
2018-05-30 06:18:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+// Write your solution in thi
+
+const driver = {name:'Sam'}
+
+function updateDriverWithKeyAndValue(driver,key,value){
+  
+ let newdriver = Object.assign({},driver,{[key]:value})
+return newdriver
+  
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  driver.key = value
+  return driver
+}
